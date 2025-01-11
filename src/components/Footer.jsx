5cd8e0018cd5c06dc35ebfcf3c0597b439248da7
Naveen Soni <naveen0929.be21@chitkara.edu.in>
2025-01-11 14:42:19 +0530

    Pricing plans section completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { FaFacebookSquare } from "react-icons/fa";
 
 const Footer = ({}) => {
   return (
-    <div className="flex justify-evenly mb-10">
+    <div className="flex justify-evenly my-10">
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../components/ui/button";
-import Services from "../components/ServiceGrid";
+import ServiceGrid from "../components/ServiceGrid";
 import laptop1 from "../assets/laptop1.png";
 import laptop2 from "../assets/laptop2.png";
 import image from "../assets/image.png";
@@ -31,7 +31,7 @@ export default function Home() {
       {/* Our Latest Work  */}
       <section className="bg-[#2b2b2b] tracking-widest rounded-[30px] py-7">
         <h3 className="text-white ml-10">Our Latest Work</h3>
-        <div className="flex justify-evenly items-center">
+        <div className="flex flex-col gap-10 xl:gap-0 xl:flex-row justify-evenly items-center">
           <div className="bg-laptop1 bg-cover rounded-xl p-5 h-[400px] flex flex-col justify-between">
             <div className="flex gap-10 top-6">
               <span className="text-white">Website Design</span>
@@ -62,8 +62,8 @@ export default function Home() {
       </section>
       {/* What We Do  */}
       <section className="bg-bgdot ">
-        <div className="tracking-widest flex max-w-[1260px] mx-auto gap-36 py-16">
-          <div className="flex flex-col justify-between w-fit">
+        <div className="tracking-widest flex flex-col items-center xl:flex-row max-w-[1260px] mx-auto gap-36 py-16">
+          <div className="flex flex-col gap-16 xl:gap-32 xl:justify-between w-fit">
             <h1 className="text-6xl font-extrabold">What do we do?</h1>
             <div>
               <h1 className="text-6xl font-extrabold tracking-widest">
@@ -104,10 +104,10 @@ export default function Home() {
       </section>
       {/* Services */}
       <section className="max-w-[1260px] mx-auto mb-12">
-        <Services />
+        <ServiceGrid />
       </section>
       {/* Our Work  */}
-      <section className="bg-[#2b2b2b] tracking-wider rounded-[30px] py-16">
+      <section className="bg-[#2b2b2b] tracking-wider rounded-[30px] p-16">
         <div className="flex justify-between max-w-[1460px] mx-auto">
           <h1 className="text-5xl tracking-widest text-white font-extrabold">
             Our Work
@@ -116,7 +116,7 @@ export default function Home() {
             Explore more of our work
           </button>
         </div>
-        <div className="flex gap-14 max-w-[1460px] my-10 mx-auto">
+        <div className="flex flex-col items-center xl:flex-row gap-14 max-w-[1460px] my-10 mx-auto">
           <div className="flex flex-col gap-6">
             <div>
               <img src={laptop1} className="h-[350px]" alt="haze productions" />
@@ -161,6 +161,67 @@ export default function Home() {
               <div className="text-white flex gap-5 font-extralight text-sm">
                 <p>Web Design</p>
                 <p>Web Development</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Pricing Plans  */}
+      <section className="p-16">
+        <h1 className="font-extrabold text-5xl mb-4">Pricing Plans</h1>
+        <p className="mb-10">
+          Tailored solutions to fit your business needs and budget.
+        </p>
+        <div className="flex flex-wrap gap-10 justify-around">
+          <div className="flex flex-col justify-between h-[600px] rounded-2xl border border-black shadow-[0_8px_0_0_black]">
+            <div>
+              <h1 className="text-5xl font-extrabold p-12">₹17,999/m</h1>
+              <ul className="list-disc space-y-5 px-12 max-w-[350px]">
+                <li className="text-lg tracking-widest">
+                  8 Posts + 4 Stories/m
+                </li>
+                <li className="text-lg tracking-widest">SMM (2 Platforms)</li>
+                <li className="text-lg tracking-widest">
+                  Basic SEO Optimization
+                </li>
+              </ul>
+            </div>
+            <div className="border w-fit border-black rounded-full m-7">
+              <Button text={"I like this"} />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between h-[600px] rounded-2xl border border-black shadow-[0_8px_0_0_black]">
+            <div>
+              <h1 className="text-5xl font-extrabold p-12">₹27,999/m</h1>
+              <ul className="list-disc space-y-5 px-12 max-w-[350px]">
+                <li className="text-lg tracking-widest">SMM 4 platforms</li>
+                <li className="text-lg tracking-widest">
+                  12 Posts + 8 Stories/m
+                </li>
+                <li className="text-lg tracking-widest">
+                  Advanced SMM Strategy with Analytics
+                </li>
+                <li className="text-lg tracking-widest">
+                  Advanced SEO + Analytics
+                </li>
+                <li className="text-lg tracking-widest">Ad Campaign Setup </li>
+              </ul>
+            </div>
+            <div className="border w-fit border-black rounded-full m-7">
+              <Button text={"I like this"} />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between h-[600px] rounded-2xl border border-black shadow-[0_8px_0_0_black]">
+            <h1 className="text-3xl font-extrabold p-12 tracking-wider">
+              Tailored for you
+            </h1>
+            <div>
+              <p className="list-disc space-y-5 px-7 text-lg max-w-[330px] tracking-widest">
+                A personalized solution to fit your unique business needs.
+                Contact us for a custom quote.
+              </p>
+              <div className="border w-fit border-black rounded-full m-7">
+                <Button text={"Schedule A Call"} />
               </div>
             </div>
           </div>
